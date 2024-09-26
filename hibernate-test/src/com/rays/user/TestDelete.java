8dd16164f77462876adda0bc14c5faabde498d27
sheCoder <77430262+madhumitarajarwal@users.noreply.github.com>
2024-09-26 18:51:11 +0530

    oky

---
+++ hibernate-test/src/com/rays/user/TestDelete.java
@@ -0,0 +1,32 @@
+package com.rays.user;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class TestDelete {
+
+	public static void main(String[] args) {
+
+		UserDTO dto=new UserDTO();
+		dto.setId(1);
+		
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.delete(dto);
+
+		tx.commit();
+
+		session.close();
+
+	}
+
+}
